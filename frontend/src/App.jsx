8485d41c43124772f6804e12a869ab84229f7bb1
EOluwaseun <oluwaseunajibowo@gmail.com>
2024-07-28 02:56:15 +0100

    file modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,13 @@ import { createContext, useEffect } from 'react';
 import { useState } from 'react';
 import { lookInSession } from './common/session';
 import Editor from './pages/editor.pages';
+import HomePage from './pages/home.page';
+
 import PublishForm from './components/publish-form.component';
+import SearchPage from './pages/search.page';
+import PageNotFound from './pages/404.page';
+import ProfilePage from './pages/profile.page';
+import BlogPage from './pages/blog.page';
 
 export const UserContext = createContext({});
 const App = () => {
@@ -25,10 +31,16 @@ const App = () => {
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
       <Routes>
         <Route path="/editor" element={<Editor />} />
+        <Route path="/editor/:blog_id" element={<Editor />} />
         <Route path="/publish" element={<PublishForm />} />
         <Route path="/" element={<Navbar />}>
-          <Route path="/sign-in" element={<UserAuthForm type={'sign-in'} />} />
-          <Route path="/sign-up" element={<UserAuthForm type={'sign-up'} />} />
+          <Route index element={<HomePage />} />
+          <Route path="sign-in" element={<UserAuthForm type={'sign-in'} />} />
+          <Route path="sign-up" element={<UserAuthForm type={'sign-up'} />} />
+          <Route path="search/:query" element={<SearchPage />} />
+          <Route path="user/:id" element={<ProfilePage />} />
+          <Route path="blog/:blog_id" element={<BlogPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </UserContext.Provider>
